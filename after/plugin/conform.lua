5095f0434a30975bace6a50f8923297e05f56f42
Cristobal Hall-Ramos <crishallramos@yahoo.com>
2024-03-03 15:45:42 -0500

    conform settings

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -16,20 +16,29 @@ require("conform").setup({
 		typescriptreact = { { "prettierd", "prettier" } },
 		vue = { { "prettierd", "prettier" } },
 		yaml = { { "prettierd", "prettier" } },
+		bash = { "beautysh" },
+		prisma = { "prisma" },
 	},
 
 	-- Set the log level. Use `:ConformInfo` to see the location of the log file.
 	log_level = vim.log.levels.WARN,
 	--     -- Conform will notify you when a formatter errors
 	notify_on_error = true,
-	format_on_save = {
-		lsp_fallback = true,
-		timeout_ms = 750,
-	},
-
+	format_on_save = function(bufnr)
+		-- Disable with a global or buffer-local variable
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 750, lsp_fallback = true }
+	end,
 	formatters = {
 		prettier = {
 			prepend_args = {},
+		},
+		prisma = {
+			command = "prisma format",
+			stdin = false,
+			require_cwd = true,
 		},
 	},
 })
@@ -62,4 +71,22 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function(args)
 		require("conform").format({ bufnr = args.buf })
 	end,
+})
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
 })
