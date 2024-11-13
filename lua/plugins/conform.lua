9d01c3ee2d3179edda2deb51b6f639feedbd1ef3
Cristobal Hall-Ramos <cristobal_hall-ramos@comcast.com>
2024-11-13 10:51:44 -0500

    various changes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,36 +2,43 @@ local config = function()
   local conform = require("conform")
 
   local standardJsOrDefault = function(bufnr)
-    if conform.get_formatter_info("standardjs", bufnr).available then
+    local cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+    -- if cwd == "platco-resident-app" then
+    --   return { lsp_format = "prefer" }
+    -- end
+
+    if vim.fn.filereadable(cwd .. "/.eslintrc.js") == 1 then
+      return { "eslint" }
+    elseif conform.get_formatter_info("standardjs", bufnr).available then
       return { "standardjs" }
     else
-      return { "prettierd", "prettier", "standardjs" }
+      return { "prettierd", "prettier" }
     end
   end
 
   conform.setup({
     formatters_by_ft = {
       lua = { "stylua" },
-      angular = { "prettierd", "prettier" },
-      css = { "prettierd", "prettier" },
-      flow = { "prettierd", "prettier" },
-      graphql = { "prettierd", "prettier" },
-      html = { "prettierd", "prettier" },
-      json = { "jq", "prettierd", "prettier" },
+      angular = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
+      flow = { "prettierd", "prettier", stop_after_first = true },
+      graphql = { "prettierd", "prettier", stop_after_first = true },
+      html = { "prettierd", "prettier", stop_after_first = true },
+      json = { "jq", "prettierd", "prettier", stop_after_first = true },
       javascript = standardJsOrDefault,
-      javascriptreact = standardJsOrDefault,
-      less = { "prettierd", "prettier" },
-      markdown = { "prettierd", "prettier" },
-      scss = { "prettierd", "prettier" },
-      typescript = { "prettierd", "prettier" },
-      typescriptreact = { "prettierd", "prettier" },
-      vue = { "prettierd", "prettier" },
-      yaml = { "yamlfmt", "prettierd", "prettier" },
+      javascriptreact = {},
+      less = { "prettierd", "prettier", stop_after_first = true },
+      markdown = { "prettierd", "prettier", stop_after_first = true },
+      scss = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      vue = { "prettierd", "prettier", stop_after_first = true },
+      yaml = { "prettierd", "prettier", stop_after_first = true },
       bash = { "beautysh" },
       prisma = { "prisma" },
       python = { "black" },
     },
-    log_level = vim.log.levels.TRACE,
+    log_level = vim.log.levels.INFO,
     notify_on_error = true,
   })
 
@@ -47,7 +54,11 @@ local config = function()
   }
 
   vim.keymap.set("n", "<leader>f", function()
-    conform.format({ async = true, lsp_fallback = true })
+    conform.format({
+      async = true,
+      lsp_fallback = true,
+      filter = function(client) return client.name ~= "ts_ls" end
+    })
   end)
 
   vim.api.nvim_create_user_command("FormatDisable", function(args)
