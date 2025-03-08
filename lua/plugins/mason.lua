49babe63f1e74e7b9ee2b541aafd8e489b897dd2
Cris Hall-Ramos <crishallramos@yahoo.com>
2025-03-08 14:05:55 -0500

    css styles

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,6 +9,15 @@ local handlers = {
       on_attach = on_attach,
     })
   end,
+  ["cssls"] = function ()
+    --Enable (broadcasting) snippet capability for completion
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    require("lspconfig").cssls.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end,
   ["ts_ls"] = function()
     require("lspconfig").ts_ls.setup({
       settings = {
