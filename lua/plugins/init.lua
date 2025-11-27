66a69909a1ac314bd420dd66c59521161cef293e
Cris Hall-Ramos <crishallramos@yahoo.com>
2025-11-27 09:22:57 -0500

    Sync with mac

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,39 +1,39 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        sync_install = false,
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-        injections = { enable = true },
-      })
-    end
-  },
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = function()
-      require('nvim-autopairs').setup({})
-    end
-  },
-  {
-    'mbbill/undotree',
-    config = function()
-      vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-      vim.opt.undofile = true
-      vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end
-  },
-  {
-    'tpope/vim-fugitive',
-    config = function()
-      vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    end
-  },
-  'alvan/vim-closetag',
-  { import = 'plugins.colors' }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+			vim.opt.undofile = true
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		end,
+	},
+	"alvan/vim-closetag",
+	{ import = "plugins.colors" },
+	"nvim-tree/nvim-web-devicons",
 }
