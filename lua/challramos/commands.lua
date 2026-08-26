faec25809da838ed7369362d378ce0ad5cb41f77
Cris Hall-Ramos <crishallramos@yahoo.com>
2026-08-26 11:36:40 -0400

    add decrypt commands

--- lua/challramos/commands.lua
+++ lua/challramos/commands.lua
@@ -9,3 +9,18 @@ vim.api.nvim_create_user_command("CodeLensToggle", function()
   end
   vim.notify("CodeLens " .. (codelens_enabled and "enabled" or "disabled"))
 end, { desc = "Toggle LSP codelens" })
+
+vim.api.nvim_create_user_command("VaultStringDecrypt", function()
+  vim.cmd("normal! gvy")
+  vim.fn.setreg('"', vim.fn.system(
+    'sed "s/^[[:space:]]*//" | ansible-vault decrypt --vault-pass-file private/.vaultpass --output -',
+    vim.fn.getreg('"')
+  ))
+  vim.cmd("normal! gvP")
+end, { range = true, desc = "Decrypt vault string inline" })
+
+vim.api.nvim_create_user_command("Base64Decode", function()
+  vim.cmd("normal! gvy")
+  vim.fn.setreg('"', vim.fn.system("base64 --decode", vim.fn.getreg('"')))
+  vim.cmd("normal! gvP")
+end, { range = true, desc = "Base64 decode selection inline" })
